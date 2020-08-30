a46c2a673ccfe66a648019738b538fd40fe6bcaf
kurvinh <khendricks@2u.com>
2020-08-30 19:19:01 +0200

    Updates

--- _nuxt/pages/test.16bc3e7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{188:function(n,t,e){"use strict";e.r(t);var l={layout:function(n){return"default"}},r=e(45),component=Object(r.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
