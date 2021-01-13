c62a5bc9fbe2b7e9f155c2b8880ef5b131176cf7
HendricksK <kurvinhendricks@gmail.com>
2021-01-13 14:25:43 +0200

    windows dev build, pages update

--- pages/index.vue
+++ pages/index.vue
@@ -1,99 +1,113 @@
 <template>
-  <div class="container" id="home">
-    <section class="section">
-      <h1 class="title">
-            Sacos Cycling History
-      </h1>
-    </section>
-    <section class="section">  
-      <div class="container">
-        <h1 class="title"></h1>
-        <div class="tile is-ancestor">
-          <div class="tile is-vertical is-12">
-            <div class="tile">
-              <div class="tile is-parent is-vertical">
-                <article class="tile is-child notification is-info">
-                  <figure class="image">
-                    <img src="https://github.com/HendricksK/sacos_images/blob/master/Basil%20Williams%20Top%20Senior%20in%201948.jpg?raw=true">
-                  </figure>
+    <div class="container" id="home">
+        <p v-if="$fetchState.pending">Fetching home</p>
+        <p v-else-if="$fetchState.error">An error occured</p> 
 
-                  <div class="accordion">
-                    <div class="col">
-                      <div class="tabs">
-                        <div class="tab">
-                          <input type="checkbox" id="chck1">
-                          <label class="tab-label" for="chck1">Basil Williams</label>
-                          <div class="tab-content">
-                            Basil Williams was one of three brothers, he was the oldest, his Brother Ralph Williams was also a cyclist the father of Michael Williams. Basil was a Champion on the Road and track. These cyclist were in the forefront of where the sport originated from. They could not compete at the highest  level due to apartheid. These are our struggle hero's.
+        <section class="section" id="heading">
+          <h1 class="title">
+                {{ heading.heading }}
+          </h1>
+        </section>
+        <section class="section" id="banner">  
+              <div class="container">
+                <h1 class="title"></h1>
+                <div class="tile is-ancestor">
+                    <div class="tile is-vertical is-12">
+                        <div class="tile">
+
+                          <div v-for="banner of banner.banner" class="tile is-parent is-vertical">
+                            <article class="tile is-child notification is-info">
+                              <figure class="image">
+                                <img :src="banner.image[0].img_url">
+                              </figure>
+
+                              <div v-if="banner.image[0].collapsable" class="accordion">
+                                <div class="col">
+                                  <div class="tabs">
+                                    <div class="tab">
+                                      <input type="checkbox" id="chck1">
+                                      <label class="tab-label" for="chck1">Basil Williams</label>
+                                      <div class="tab-content">
+                                        {{banner.image[0].data}}
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <p v-else class="subtitle">{{banner.image[0].data}}</p>
+
+                            </article>
                           </div>
                         </div>
-                      </div>
                     </div>
-                  </div>
-
-
-                </article>
-              </div>
-              <div class="tile is-parent is-vertical">
-                <article class="tile is-child notification is-info">
-                  <figure class="image">
-                    <img src="https://github.com/HendricksK/sacos_images/blob/master/Mark%20Hendricks%20Winning%20Last%20Stage%20of%20Tour%20de%20Boland.jpg?raw=true">
-                  </figure>
-                  <p class="subtitle">Mark Hendricks Winning Last Stage of Tour de Boland</p>
-                </article>
-              </div>
-              <div class="tile is-parent is-vertical">
-                <article class="tile is-child notification is-info">
-                  <figure class="image">
-                    <img src="https://github.com/HendricksK/sacos_images/blob/master/Tessel%20Crotz%20Vygekraal%20staduim.jpg?raw=true">
-                  </figure>
-                  <p class="subtitle">Our Worcester Champion Tessel Crotz a family of Cycling Hero's</p>
-                </article>
-              </div>
-              <div class="tile is-parent is-vertical">
-                <article class="tile is-child notification is-info">
-                  <figure class="image">
-                    <img src="https://github.com/HendricksK/sacos_images/blob/master/Tessel%20Crotz%20WC%20Hero.jpg?raw=true">
-                  </figure>
-                  <p class="subtitle">Our Worcester Champion Tessel Crotz a family of Cycling Hero's</p>
-                </article>
+                </div>
+            </div>
+        </section>
+        <section class="section" id="content">
+          <div class="columns is-mobile">
+            <div class="column">
+              <div class="description">
+                <span class="content" v-html="content.content"></span>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-  </section>
-  <section class="section" id="contact">
-    <div class="columns is-mobile">
-      <div class="column">
+          <div class="links">
+            <a :href="'mailto:' + content.contact" target="_blank" rel="noopener noreferrer" class="button--sacos_yellow">{{content.contact}}</a>
+          </div>
+        </section>
+
         
-        <div class="description">
-          We are trying to start a historical log of the SACOS cycling team, if you have anything to contribute, please contact the below email.
-          <p class="three-quater-width">
-            When emailing, please add a return email address, the years for which the data / images / articles you are sending cover.
-            Your personal contact information will not be shared on the website, or used for anything other than verifying information if required.
-          </p>
-        </div>
-        <div class="links">
-          <a
-            href="mailto:sacoshistoricalsite@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="button--sacos_yellow"
-          >
-          sacoshistoricalsite@gmail.com
-          </a>
-        </div>
-      </div>
+
     </div>
-  </section>
-  </div>
 </template>
 
 <script>
   export default {
     layout(context) {
         return 'default'
+    },
+    data() {
+      return {
+        baseurl: process.env.baseUrl,
+        page: {},
+        heading: {},
+        banner: {},
+        content: {}
+      }
+    },
+    async fetch() {
+      
+      var heading, banner, content
+
+      heading = await fetch(
+        process.env.apiUrl + '/' + process.env.postUrl + '1' + '/heading' 
+      ).then(
+        res => res.json()
+      ),
+      banner = await fetch(
+        process.env.apiUrl + '/' + process.env.postUrl + '1' + '/banner' 
+      ).then(
+        res => res.json()
+      ),
+      content = await fetch(
+        process.env.apiUrl + '/' + process.env.postUrl + '1' + '/content' 
+      ).then(
+        res => res.json()
+      )
+
+      // Still trying to learn JSON marshalling in GO, until then I will be using JSON.parse on front end not ideal but will work
+
+      this.heading = JSON.parse(heading.Page_data_string)
+      this.banner = JSON.parse(banner.Page_data_string)
+      this.content = JSON.parse(content.Page_data_string)
+
+      // console.log(this.content)
+
+    },
+    methods: {
+      refresh() {
+        this.$fetch()
+      }
     }
   }
 </script>
