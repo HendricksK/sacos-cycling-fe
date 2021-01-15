63f2838eb779daff993aa764b7760175a3fb5e8c
HendricksK <kurvinhendricks@gmail.com>
2021-01-15 22:45:08 +0200

    https://trello.com/c/R8O2Rw1y added slider to home page

---
+++ pages/manager/managers.vue
@@ -0,0 +1,41 @@
+<template>
+  	<div>
+  		<p v-if="$fetchState.pending">Fetching articles</p>
+		  <p v-else-if="$fetchState.error">An error occured</p>
+  		<br>
+        
+  		<h1>Articles</h1>
+	    <ul>
+	      	<li v-for="article of articles">
+	      		<a :href="articleuri+'/'+article.Id">{{ article.Name }} - {{article.Author}}</a>
+	      	</li>
+	    </ul>
+  	</div>
+</template>
+
+<script>
+  export default {
+    layout(context) {
+        return 'default'
+    },
+    data() {
+    	return {
+    		baseurl: process.env.baseUrl,
+    		articleuri: process.env.articleUri,
+    		articles: []
+    	}
+    },
+    async fetch() {
+    	this.articles = await fetch(
+    		process.env.apiUrl+'/articles'
+    	).then(
+    		res => res.json()
+    	)
+    },
+    methods: {
+    	refresh() {
+    		this.$fetch()
+    	}
+    }
+  }
+</script>
