1f5e014716e4f4d6ed32686ce17a8c36c1fca791
kurvinh <kurvinhendricks@gmail.com>
2021-01-16 10:58:13 +0200

    Updates

---
+++ _nuxt/static/1610787470/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002Ftrack\u002Ftracks","\u002Fclub\u002Fclubs","\u002Fmanager\u002Fmanagers","\u002Farticle\u002Farticles","\u002Frider\u002Friders","\u002Ftest","\u002F","\u002Farticle\u002F1"]})
